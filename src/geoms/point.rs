7ee4dc4d3bd329c7a1667e3d91d99467dd44abbe
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2025-05-11 12:32:33 +0200

    add method signatures for point

--- src/geoms/point.rs
+++ src/geoms/point.rs
@@ -8,9 +8,19 @@ pub struct Point {
 }
 
 impl Point {
-    /// create a default point with coordinates x: 0.0 and y: 0.0
+    /// create a point with given coordinates
+    pub fn new(x: f64, y: f64) -> Point {
+        Point { x: x, y: y }
+    }
+    
+    // create a default point with coordinates x: 0.0 and y: 0.0
     pub fn origin() -> Point {
         Point { x: 0.0, y: 0.0 }
+    }
+
+    // create Point from WKT
+    pub fn from_WKT(wkt_string: &str) -> Point {
+        todo!()
     }
 
     /// get the x coordinate of the point
@@ -20,6 +30,14 @@ impl Point {
     /// get the y coordinate of the point
     pub fn get_y(&self) -> f64 {
         self.y
+    }
+    /// rotate the point around another point by an angle 
+    pub fn rotate(&mut self, rotation_center: Point, angle: f64) {
+        todo!()
+    }
+    /// calculate the 2D distance to another point
+    pub fn distance2D(&self, another: Point) -> f64 {
+        todo!()
     }
 }
 
