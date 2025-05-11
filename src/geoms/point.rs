78b0636306f5a573fc60b2d071f6500bb1d3b364
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2025-05-11 20:28:36 +0200

    add tests for Point

--- src/geoms/point.rs
+++ src/geoms/point.rs
@@ -13,13 +13,13 @@ impl Point {
         Point { x: x, y: y }
     }
     
-    // create a default point with coordinates x: 0.0 and y: 0.0
+    /// create a default point with coordinates x: 0.0 and y: 0.0
     pub fn origin() -> Point {
         Point { x: 0.0, y: 0.0 }
     }
 
-    // create Point from WKT
-    pub fn from_WKT(wkt_string: &str) -> Point {
+    /// create Point from WKT
+    pub fn from_WKT(wkt_string: &str) -> Result<Point, &'static str> {
         todo!()
     }
 
@@ -57,5 +57,52 @@ mod tests {
         assert_eq!(result.get_x(),0.0);
         assert_eq!(result.get_y(),0.0);
 
+    }
+    #[test]
+    fn test_creation_new() {
+        let result = Point::new(14.45, 4.312);
+        assert_eq!(result.x, 14.45);
+        assert_eq!(result.y, 4.312);
+    }
+    #[test]
+    fn test_get_coords_new() {
+        let result = Point::new(-5.8, 0.657);
+        assert_eq!(result.get_x(),-5.8);
+        assert_eq!(result.get_y(),0.657);
+
+    }
+    #[test]
+    fn test_creation_wkt_int() {
+        let result = Point::from_WKT("POINT (34 2)").unwrap();
+        assert_eq!(result.x, 0.0);
+        assert_eq!(result.y, 0.0);
+    }
+
+    #[test]
+    fn test_creation_wkt_small_int() {
+        let result = Point::from_WKT("point (34 2)").unwrap();
+        assert_eq!(result.x, 34.0);
+        assert_eq!(result.y, 2.0);
+    }
+
+    #[test]
+    fn test_creation_wkt_dbl() {
+        let result = Point::from_WKT("POINT (34.98 -8.543)").unwrap();
+        assert_eq!(result.x, 34.98);
+        assert_eq!(result.y, -8.543);
+    }
+
+
+    #[test]
+    fn test_get_coords_wkt() {
+        let result = Point::from_WKT("POINT (-3.14 9.15)").unwrap();
+        assert_eq!(result.get_x(),0.0);
+        assert_eq!(result.get_y(),0.0);
+
+    }
+    #[test]
+    fn test_creation_wkt_fail() {
+        let result = Point::from_WKT("POINT (34, 2)");
+        assert!(result.is_err());
     }
 }
