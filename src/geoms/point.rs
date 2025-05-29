47ed67156ac648954325221e82ffc6ca926d6905
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2025-05-29 09:22:16 +0200

    Implement from_wkt and to_wkt functions

--- src/geoms/point.rs
+++ src/geoms/point.rs
@@ -1,4 +1,6 @@
-/// Struct for a 2D Point
+use regex::Regex;
+
+// Struct for a 2D Point
 #[derive(Debug)]
 pub struct Point {
    /// x coordinate of the point
@@ -19,9 +21,18 @@ impl Point {
     }
 
     /// create Point from WKT
-    pub fn from_WKT(wkt_string: &str) -> Result<Point, &'static str> {
-        todo!()
+    pub fn from_wkt(wkt_string: &str) -> Result<Point, &'static str> {
+    let re = Regex::new(r"(?i)point\s*\(\s*([+-]?\d+\.?\d*)\s+([+-]?\d+\.?\d*)\s*\)").unwrap(); // (?i)point\s*\(\s*([+-]?\d*\.?\d+)\s+([+-]?\d*\.?\d+)\s*\)
+    let caps = re.captures(wkt_string.trim()).ok_or("Invalid WKT format")?;
+    let x: f64 = caps.get(1).ok_or("Missing X coordinate")?.as_str().parse().map_err(|_| "Invalid X value")?;
+    let y: f64 = caps.get(2).ok_or("Missing Y coordinate")?.as_str().parse().map_err(|_| "Invalid Y value")?;
+    Ok(Point { x, y })
     }
+
+    /// pub get WKT (well-known text) representation of a 2D point
+    pub fn to_wkt(&self) -> String {
+        return format!("POINT ({} {})", self.x, self.y);
+    } 
 
     /// get the x coordinate of the point
     pub fn get_x(&self) -> f64 {
@@ -73,21 +84,21 @@ mod tests {
     }
     #[test]
     fn test_creation_wkt_int() {
-        let result = Point::from_WKT("POINT (34 2)").unwrap();
-        assert_eq!(result.x, 0.0);
-        assert_eq!(result.y, 0.0);
+        let result = Point::from_wkt("POINT (34 2)").unwrap();
+        assert_eq!(result.x, 34.0);
+        assert_eq!(result.y, 2.0);
     }
 
     #[test]
     fn test_creation_wkt_small_int() {
-        let result = Point::from_WKT("point (34 2)").unwrap();
+        let result = Point::from_wkt("point (34 2)").unwrap();
         assert_eq!(result.x, 34.0);
         assert_eq!(result.y, 2.0);
     }
 
     #[test]
     fn test_creation_wkt_dbl() {
-        let result = Point::from_WKT("POINT (34.98 -8.543)").unwrap();
+        let result = Point::from_wkt("POINT (34.98 -8.543)").unwrap();
         assert_eq!(result.x, 34.98);
         assert_eq!(result.y, -8.543);
     }
@@ -95,15 +106,49 @@ mod tests {
 
     #[test]
     fn test_get_coords_wkt() {
-        let result = Point::from_WKT("POINT (-3.14 9.15)").unwrap();
-        assert_eq!(result.get_x(),0.0);
-        assert_eq!(result.get_y(),0.0);
+        let result = Point::from_wkt("POINT (-3.14 9.1548595)").unwrap();
+        assert_eq!(result.get_x(),-3.14);
+        assert_eq!(result.get_y(),9.1548595);
 
     }
     #[test]
-    fn test_creation_wkt_fail() {
-        let result = Point::from_WKT("POINT (34, 2)");
+    fn test_creation_wkt_fail_comma() {
+        let result = Point::from_wkt("POINT (34, 2)");
         assert!(result.is_err());
     }
+    #[test]
+    fn test_creation_wkt_fail_invalid_wkt() {
+        let wkt = "POINTS ( 5 4)";
+        let result = Point::from_wkt(wkt);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Invalid WKT format");
+    }
+
+    #[test]
+    fn test_creation_wkt_fail_invalid_x() {
+        let wkt = "POINT (test 2)";
+        let result = Point::from_wkt(wkt);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Invalid WKT format");
+    }
+    #[test]
+    fn test_creation_wkt_fail_invalid_y() {
+        let wkt = "POINT (98 test)";
+        let result = Point::from_wkt(wkt);
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Invalid WKT format");
+    }
+    #[test]
+    fn test_to_wkt_origin() {
+        let orig = Point::origin();
+        assert_eq!("POINT (0 0)", orig.to_wkt());
+    }
+
+    #[test]
+    fn test_to_wkt_other_point() {
+        let orig = Point::new(-9.4874, 67598.58548);
+        assert_eq!("POINT (-9.4874 67598.58548)", orig.to_wkt());
+    }
+
     // tests with distance and rotate
 }
--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,10 @@ fn main() {
     let a = Point::origin();
     println!("a: {a:?}");
     println!("{0}",a.get_x());
+    let b = Point::from_wkt("POINT (-65 7.5875)");
+    let b_point = b.unwrap();
+    let x = b_point.get_x(); 
+    let y = b_point.get_y(); 
+    println!("{}", b_point.to_wkt());
+
 }
